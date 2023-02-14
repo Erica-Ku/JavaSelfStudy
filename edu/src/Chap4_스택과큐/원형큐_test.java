95a9316fff8b37e114c6b9260a4fd7dd2e6cb898
Erica-Ku <yeskuminji@naver.com>
2023-02-14 13:10:13 +0900

    (5.1)

--- edu/src/Chap4_스택과큐/원형큐_test.java
+++ edu/src/Chap4_스택과큐/원형큐_test.java
@@ -34,15 +34,14 @@ class Point_2 {
 public class 원형큐_test {
 
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
+		try(Scanner stdIn = new Scanner(System.in)) {
 		원형큐 s = new 원형큐(8); // 최대 8개를 push할 수 있는 큐
 		Random random = new Random();
 		int rndx = 0, rndy = 0;
 		Point p = null;
 		while (true) {
 			System.out.println(); // 메뉴 구분을 위한 빈 행 추가
-			System.out.printf("현재 데이터 개수: %d / %d\n", s.size(), s.getCapacity());
-			System.out.print("(1)push　(2)pop　(3)peek　(4)dump　(0)종료: ");
+			System.out.print("(1)push　(2)pop　(3)dump (0)종료: ");
 
 			int menu = stdIn.nextInt();
 			if (menu == 0)
@@ -50,12 +49,12 @@ public class 원형큐_test {
 
 			switch (menu) {
 			case 1: // 푸시
-				System.out.print("데이터: ");
 				rndx = random.nextInt() % 20;
 				rndy = random.nextInt() % 20;
 				p = new Point(rndx,rndy);
 				try {
 					s.push(p);
+					System.out.println("push한 데이터는 " + p + "입니다.");
 				} catch(원형큐.Overflow원형큐Exception e) {
 					System.out.println("큐가 가득 찼습니다.");
 				}
@@ -70,19 +69,11 @@ public class 원형큐_test {
 				}
 				break;
 
-			case 3: // 피크
-				try {
-					p = s.peek();
-					System.out.println("peek한 데이터는 " + p + "입니다.");
-				} catch (원형큐.Empty원형큐Exception e) {
-					System.out.println("큐가 비어 있습니다.");
-				}
-				break;
-
-			case 4: // 덤프
+			case 3: // 덤프
 				s.dump();
 				break;
 			}
+		}
 		}
 	}
 }
